6f143b6597dfb55b08b80b39bff1c42a91c5debb
Dan Brown <dbrown.email@gmail.com>
2016-12-01 19:30:11 +0000

    Started creating tests

---
+++ src/app/pipes/filter.pipe.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed } from '@angular/core/testing';
+import { FilterPipe } from "./filter.pipe";
+
+describe('Filter Pipe Tests', () => {
+    let pipe: FilterPipe;
+
+    beforeEach(() => {
+        pipe = new FilterPipe();
+    });
+
+    let input: any = [
+        { id: 1, name: 'Dashboard' },
+        { id: 2, name: 'Settings' },
+        { id: 3, name: 'Help' }
+    ];
+
+    let output: any = [
+        { id: 2, name: 'Settings' }
+    ];
+
+    it('Should filter an object by a search term', () => {
+        var result = pipe.transform(input, 'sett');
+
+        expect(result).toEqual(output);
+    });
+});
