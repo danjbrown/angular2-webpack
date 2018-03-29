f17c5e86dcafaca183e6e4dafe520fc20c824903
Dan Brown <dbrown.email@gmail.com>
2018-03-29 12:16:19 +0100

    Update: compress Uglify warnings

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -26,6 +26,9 @@ module.exports = webpackMerge(commonConfig, {
     new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
       mangle: {
         keep_fnames: true
+      },
+      compress: {
+        warnings: false
       }
     }),
     new ExtractTextPlugin('[name].[hash].css'),
